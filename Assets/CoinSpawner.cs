9ba6058577f15a7992f95f6cee092657d8e54e44
Ali Hammoud <ali.hammoud.lb@gmail.com>
2017-03-20 20:40:31 +0200

    Coins now spawn

---
+++ Assets/CoinSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour {
+
+	public int coinsInStack = 15;
+	public GameObject coin;
+
+	private GameObject[] coinStack;
+
+	void Start () {
+
+		coinStack = new GameObject[coinsInStack];
+
+		for (int i = 0; i < coinsInStack; i ++) {
+
+			coinStack [i] = SpawnCoin();
+			coinStack [i].transform.Translate (
+				0.18f * i, 
+				0, 
+				0 
+			);
+
+		}
+
+	}
+
+	private GameObject SpawnCoin () {
+
+		return Instantiate(coin);
+
+	}
+
+	void Update () {
+		
+	}
+}
